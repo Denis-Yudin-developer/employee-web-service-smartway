1bf49b0dabf8c0c58844cb5cc82d4c8a7b88de04
lty2107 <rng48322@gmail.com>
2023-03-19 17:53:51 +0300

    Добавил интерфейс для слоя сервиса

---
+++ internal/service/service.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"smartway-test-task/internal/model"
+	"smartway-test-task/internal/repository"
+)
+
+type Employee interface {
+	Create(employee model.Employee) (int, error)
+	GetAll() ([]model.Employee, error)
+	GetById(id int) (model.Employee, error)
+	GetAllByCompany(companyId int) ([]model.Employee, error)
+	GetAllByDepartment(department string) ([]model.Employee, error)
+	Update(updatedEmployee model.UpdateEmployee, employeeId int) error
+	Delete(id int) error
+}
+
+type Service struct {
+	Employee
+}
+
+func NewService(repos *repository.Repository) *Service {
+	return &Service{
+		Employee: NewEmployeeService(repos),
+	}
+}
